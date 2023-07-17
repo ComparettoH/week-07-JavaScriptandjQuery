6b1ac1e79986bfe008354f36862c23171fcce316
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-07-17 13:46:02 -0500

    create html, css, jq, js files and test

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log ('test 654')
+
+$(document).ready(onReadySetGo);
+
+function onReadySetGo (){
+console.log('test 01234')
+// Listener/Handler
+$("#gButton").on('click', makeDivs)
+
+
+
+
+}
+
+// function that makes Divs
+function makeDivs (){
+    
+}
